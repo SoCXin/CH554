2625bee231fe14aa6068efe89f90e57b6b3674f5
qitas <qitas@qitas.cn>
2019-06-23 21:20:30 +0800

    change way

--- src/SDCC/examples/ws2812/bitbang.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void bitbangSetup();
-void bitbangWs2812(uint8_t ledCount, __xdata uint8_t * ledData);
--- src/SDCC/include/bootloader.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define BOOT_ADDR  0x3800
-void (* __data bootloader)(void) = BOOT_ADDR;
