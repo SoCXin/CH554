1841fd47b2f1e91acb645366f98106a2ba2f9547
qitas <qitas@qitas.cn>
2021-03-09 10:28:34 +0800

    fix

--- examples/blink/main.c
+++ examples/blink/main.c
@@ -6,7 +6,8 @@
 #define LED_PIN 7
 SBIT(LED, 0x90, LED_PIN);
 
-void main() {
+void main()
+{
     CfgFsys();
 
     // Configure pin 1.7 as GPIO output
@@ -14,8 +15,9 @@ void main() {
     P1_MOD_OC = P1_MOD_OC & ~(1<<LED_PIN);
     P1_DIR_PU = P1_DIR_PU |	(1<<LED_PIN);
 
-    while (1) {
-    	mDelaymS(100);
+    while (1)
+    {
+        mDelaymS(100);
         LED = !LED;
     }
 }
